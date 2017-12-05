69b217ef51a2772b6d8a22403a9622517cf3fedf
Tien Nguyen <tiennguyen.ftu.k52@gmail.com>
2017-12-06 00:56:03 +0700

    add prettier command

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import {render} from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 
-const App = () => (
-  <div>Hello World</div>
-);
+const App = () => <div>Hello World</div>;
 
 render(<App />, document.getElementById('app'));
