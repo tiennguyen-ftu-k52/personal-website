dbc05935daf6509d98c15860696b7ae853c214e2
Tien Nguyen <tiennguyen.ftu.k52@gmail.com>
2017-12-15 00:40:34 +0700

    improve production webpack config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,6 +4,10 @@ import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import StatsPlugin from 'stats-webpack-plugin';
+import StyleExtHtmlWebpackPlugin from 'style-ext-html-webpack-plugin';
+import ScriptExtHtmlWebpackPlugin from 'script-ext-html-webpack-plugin';
+import CompressionPlugin from 'compression-webpack-plugin';
+import autoprefixer from 'autoprefixer';
 
 import baseConfig from './webpack.config.base';
 
@@ -14,36 +18,79 @@ const paths = {
 };
 
 export default merge.smart(baseConfig, {
+  devtool: 'cheap-module-source-map',
   output: {
-    filename: '[name]-[hash].min.js',
+    filename: '[name]-[chunkhash].min.js',
   },
   plugins: [
-    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {NODE_ENV: JSON.stringify('production')},
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.[chunkhash].js',
+      minChunks(module) {
+        return module.context && module.context.indexOf('node_modules') >= 0;
+      },
+    }),
+    new webpack.HashedModuleIdsPlugin(),
     new HtmlWebpackPlugin({
       template: path.join(paths.SRC, 'index.html'),
+      excludeChunks: ['base'],
       filename: 'index.html',
-      inject: 'body',
-    }),
-    new ExtractTextPlugin('[name]-[hash].min.css'),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false,
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+      },
     }),
     new webpack.optimize.UglifyJsPlugin({
-      beautify: false,
-      compressor: {
+      compress: {
         warnings: false,
         screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+    new webpack.HashedModuleIdsPlugin(),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new ExtractTextPlugin({
+      filename: '[name].[contenthash].css',
+      allChunks: true,
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'defer',
+    }),
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [
+          autoprefixer({
+            browsers: ['last 3 version', 'ie >= 10'],
+          }),
+        ],
       },
     }),
     new StatsPlugin('webpack.stats.json', {
       source: false,
       modules: false,
     }),
-    new webpack.DefinePlugin({
-      'process.env': {NODE_ENV: JSON.stringify('production')},
-    }),
-    require('autoprefixer')
   ],
 });
